c41530dec97bbbd699ce6f37761b9f5f724150a2
Jack Pachon <jack.kilian3592@gmail.com>
2026-08-23 22:03:48 -0500

    Se soluciona los conflictos de git

--- src/hooks/useActivity.js
+++ src/hooks/useActivity.js
@@ -1,11 +1,4 @@
 import { useState } from "react";
-<<<<<<< HEAD
-import activityEngineDropPencilOnFigure from "../core/activityEngine/precesses/activityEngineDropPencilOnFigure";
-import { checkActivity } from "../core/activityEngine";
-import useActivity03 from "./useActivity03";
-import isCompleted from "../core/activityEngine/utils/isCompleted";
-=======
->>>>>>> 73edd8c (se hizo la refactorizacion de las tres actividades, para que la logica estubiera fuera de useactivity)
 
 const useActivity = (activityData) => {
   const [state, setState] = useState({
@@ -14,84 +7,19 @@ const useActivity = (activityData) => {
     correct: false,
   });
 
-<<<<<<< HEAD
-  const {
-    selectedState,
-    actions: { selectActivity03Figure, checkActivity03, resetActivity03 },
-  } = useActivity03(activityData);
-
-  const { selectedFaces } = selectedState;
-
-  const selectFigure = (figureId) => {
-    setState((previousState) => {
-      const figureAlreadySelected =
-        previousState.selectedFigures.includes(figureId);
-
-      if (figureAlreadySelected) {
-        return previousState;
-      }
-
-      const updatedFigures = [...previousState.selectedFigures, figureId];
-      const completed = isCompleted(
-        updatedFigures.length,
-        activityData.figures.length,
-      );
-
-      return {
-        ...previousState,
-        selectedFigures: updatedFigures,
-        paintedFigures: updatedFigures,
-        completed,
-      };
-    });
-  };
-
-  const selectPencil = (pencilId) => {
-=======
   const completeActivity = (completed) => {
->>>>>>> 73edd8c (se hizo la refactorizacion de las tres actividades, para que la logica estubiera fuera de useactivity)
     setState((previousState) => ({
       ...previousState,
       completed,
     }));
   };
 
-<<<<<<< HEAD
-  const dropPencilOnFigure = (pencilId, figureId) => {
-    setState((previousState) => {
-      return activityEngineDropPencilOnFigure(
-        previousState,
-        activityData.pencils,
-        activityData.figures,
-        pencilId,
-        figureId,
-      );
-    });
-  };
-
-  const submit = () => {
-    setState((previousState) => {
-      /*   const newState = checkActivity({
-        state: previousState,
-        data: activityData,
-      }); */
-
-      const correct = checkActivity03();
-
-      return {
-        ...previousState,
-        submitted: true,
-        correct,
-      };
-    });
-=======
   const submit = (correct) => {
     setState((previousState) => ({
       ...previousState,
       submitted: true,
       correct,
     }));
->>>>>>> 73edd8c (se hizo la refactorizacion de las tres actividades, para que la logica estubiera fuera de useactivity)
   };
 
   const reset = () => {
@@ -111,10 +39,6 @@ const useActivity = (activityData) => {
   return {
     data: activityData,
     state,
-<<<<<<< HEAD
-    selectedState,
-=======
->>>>>>> 73edd8c (se hizo la refactorizacion de las tres actividades, para que la logica estubiera fuera de useactivity)
     actions,
   };
 };
