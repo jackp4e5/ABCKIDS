589a7453183dbdacab8a809ffb59fcf6909d895a
Jack Pachon <jack.kilian0196@gmail.com>
2026-09-01 15:49:57 -0500

    se instala polyfill

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
 import "./styles/reset.css";
 import "./styles/variables.css";
 import "./styles/globals.css";
 
-createRoot(document.getElementById('root')).render(
+import { polyfill } from "mobile-drag-drop";
+
+
+
+polyfill();
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
