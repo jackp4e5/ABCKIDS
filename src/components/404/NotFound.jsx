b28e79d7de1e370a429dcfd80330ae05791bfdce
Jack Pachon <jack.kilian3592@gmail.com>
2026-09-04 20:16:08 -0500

    Se agraga la pagina 404

--- src/components/404/NotFound.jsx
+++ src/components/404/NotFound.jsx
@@ -1,29 +1,17 @@
-import React from 'react';
-import './NotFound.css';
-import back from "../../../public/images/Activity03/hero.png"
+import { Link } from "react-router-dom";
+import "./NotFound.css";
+const hero = "/images/FrontPage/hero.png"
+const back = "/images/return.png"
 export const NotFound = () => {
   return (
-    <div className="slide-wrapper">
-      <div className="slide-container">
-        {/* Imagen de fondo */}
-        <img 
-          src={back}
-          alt="Fondo de la actividad" 
-          className="slide-bg" 
-        />
-
-        {/* Elemento / Actividad posicionada en % respecto al fondo */}
-        <div className="activity-item item-button" style={{ top: '76%', left: '75%' }}>
-          <button onClick={() => alert('¡Actividad iniciada!')}>
-            Grupo de 23 objetos
-          </button>
-        </div>
-
-        {/* Otra actividad o texto */}
-        <div className="activity-item item-text" style={{ top: '16%', left: '26%' }}>
-          <h2>Nociones Espaciales</h2>
-        </div>
-      </div>
+    <div className="container notFound">
+      <img src={hero} className='' alt="image hero" />
+      <p>404</p>
+      <p>Este lugar no esta disponible</p>
+      <Link className="btnReturn" to={"/"}>
+      <img src={back} alt="" />
+      
+      </Link>
     </div>
   );
 };
